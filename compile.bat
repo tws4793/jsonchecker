1ee3f592bd80d91a0f60d995fe706dd66dee85b5
TEO WEI SHEN _ <weishen.teo.2015@smu.edu.sg>
2017-02-10 18:40:27 +0800

    Revert "Improved version (10/02/2017)"
    This reverts commit ee64a3c422cb3c63be9bca051968b524c835be29.

--- compile.bat
+++ compile.bat
@@ -1,10 +1,3 @@
-@echo off
-
-set dirClasses=classes
-
-if not exist %dirClasses%\NUL mkdir %dirClasses%
-javac -d %dirClasses% -classpath lib/* src/*.java
-jar -cvf lib/JSONChecker.jar -C %dirClasses% .
-
-@echo on
+javac -d classes -classpath "lib/*" src/*.java
+jar -cvf lib/JSONChecker.jar -C classes .
 pause
